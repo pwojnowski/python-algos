a4c8f4f591636a05490b7dca5483eb2200d3657c
Przemysław Wojnowski <esperanto@cumego.com>
2023-10-15 23:31:03 +0200

    Number factors - bottom up approach

--- algos/dp.py
+++ algos/dp.py
@@ -1,4 +1,16 @@
 def number_factor_dp(x: int, factors: list):
-    """Calculate the number of distinct factors of number X, obtained from given numbers."""
+    """Find the number of ways to express x as the sum of given factors."""
+    # idx 0 = 1, because it's the number of times a factor equal i can fit in i.
+    # Another words: a number can contains itself once.
+    count = [1, 1, 1, 2]
+    if x == 0:
+        return 0
     if x <= 3:
-        return 1
+        return count[x]
+    for i in range(4, x + 1):
+        total = 0
+        for f in factors:
+            if i >= f:
+                total += count[i-f]
+        count.append(total)
+    return count[x]
--- tests/test_dp.py
+++ tests/test_dp.py
@@ -5,6 +5,15 @@ from algos.dp import number_factor_dp
 class DynamicProgrammingTest(TestCase):
 
     def test_number_factor(self):
-        self.assertEqual(1, number_factor_dp(1, [1, 3, 4]))
-        self.assertEqual(1, number_factor_dp(2, [1, 3, 4]))
-        self.assertEqual(1, number_factor_dp(3, [1, 3, 4]))
+        factors = [1, 3, 4]
+        self.assertEqual(0, number_factor_dp(0, factors))
+
+        self.assertEqual(1, number_factor_dp(1, factors))
+        self.assertEqual(1, number_factor_dp(2, factors))
+
+        self.assertEqual(2, number_factor_dp(3, factors))
+        self.assertEqual(3, number_factor_dp(4, [1, 3, 5]))
+        self.assertEqual(4, number_factor_dp(4, factors))
+        self.assertEqual(6, number_factor_dp(5, factors))
+        self.assertEqual(9, number_factor_dp(6, factors))
+        self.assertEqual(15, number_factor_dp(7, factors))
